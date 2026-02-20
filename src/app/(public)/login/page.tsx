6fd6e860f6db81b5912d4b7df0de4e434b030a77
David Almeida <contato.davidalmeida@outlook.com>
2026-02-20 02:33:54 -0300

    ✅ Add: Login and Register pages

---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <>
+      <p>Página de Acesso ao Sistema.</p>
+      <span>
+        Voltar para <a href="/" className="text-lg text-violet-900 font-bold font-ubuntu">página inicial</a>
+      </span>
+    </>
+  );
+}
---
+++ src/app/(public)/register/page.tsx
@@ -0,0 +1,13 @@
+export default function Register() {
+  return (
+    <>
+      <p>Páginas de Registro de Conta.</p>
+      <span>
+        Voltar para{" "}
+        <a href="/" className="text-lg text-violet-900 font-bold font-ubuntu">
+          página inicial
+        </a>
+      </span>
+    </>
+  );
+}
