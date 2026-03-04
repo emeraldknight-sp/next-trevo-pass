9e4616ceb100abfe710777a0839876abbf1e6e54
David Almeida <contato.davidalmeida@outlook.com>
2026-03-04 10:55:47 -0300

    ✅ Add: auth and user services

---
+++ src/services/user.service.ts
@@ -0,0 +1,31 @@
+import { db } from "@/lib/firebase";
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore";
+
+export const createUserProfile = async (
+  uid: string,
+  name: string,
+  email: string,
+  cpf: string,
+  phone: string | null,
+) => {
+  await setDoc(doc(db, "users", uid), {
+    uid,
+    name,
+    email,
+    cpf,
+    phone,
+    points: 0,
+    totalPointsEarned: 0,
+    level: "bronze",
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  });
+  return;
+};
+
+export const getUserProfile = async (uid: string) => {
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+
+  return docSnap.exists() ? docSnap.data() : null;
+};
