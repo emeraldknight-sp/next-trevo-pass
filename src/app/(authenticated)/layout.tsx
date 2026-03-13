5fadbfbf670667286868ef8e8eba053b8d44a2a3
David Almeida <contato.davidalmeida@outlook.com>
2026-03-13 13:42:23 -0300

    ✅ Update: AuthLayout component adding TabBar component

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,5 +1,6 @@
 import { ProtectedRoute } from "@/components/auth/protected-route";
-import Link from "next/link";
+import { Container } from "@/components/layout";
+import { TabBar } from "@/components/layout/tab-bar";
 
 export default function AuthenticatedLayout({
   children,
@@ -8,37 +9,10 @@ export default function AuthenticatedLayout({
 }) {
   return (
     <ProtectedRoute>
-      {children}
-      <div className="flex flex-row justify-between gap-3 p-4">
-        <Link
-          href="/dashboard"
-          className="flex flex-col justify-center items-center"
-        >
-          <img src="/icons/home.png" className="w-8 h-8" />
-          <span className="text-xs text-violet-900 font-medium">Home</span>
-        </Link>
-        <Link
-          href="/points"
-          className="flex flex-col justify-center items-center"
-        >
-          <img src="/icons/points.png" className="w-8 h-8" />
-          <span className="text-xs text-violet-900 font-medium">Pontos</span>
-        </Link>
-        <Link
-          href="/rewards"
-          className="flex flex-col justify-center items-center"
-        >
-          <img src="/icons/rewards.png" className="w-8 h-8" />
-          <span className="text-xs text-violet-900 font-medium">Resgates</span>
-        </Link>
-        <Link
-          href="/profile"
-          className="flex flex-col justify-center items-center"
-        >
-          <img src="/icons/profile.png" className="w-8 h-8" />
-          <span className="text-xs text-violet-900 font-medium">Perfil</span>
-        </Link>
-      </div>
+      <Container>
+        {children}
+        <TabBar />
+      </Container>
     </ProtectedRoute>
   );
 }
