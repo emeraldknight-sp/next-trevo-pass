698a2c08dd45fee21b5211b45b2875b5f8b3e36c
David Almeida <contato.davidalmeida@outlook.com>
2026-03-28 20:27:57 -0300

    ✅ Update: Auth controller

--- src/features/auth/controllers/auth.controller.ts
+++ src/features/auth/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
-import { UserForm } from "@/@types";
 import { createAuthService } from "../services/auth.service";
 
 export async function createAuthController(email: string, password: string) {
   try {
     const { uid } = await createAuthService(email, password);
     return { uid };
-  } catch (error: any) {
+  } catch (error: unknown) {
     throw error;
   }
 }
