1395299f5ca77acad14ae7c522f7842eac046daf
David Almeida <contato.davidalmeida@outlook.com>
2026-03-27 23:17:19 -0300

    🏷️ Update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,26 +1,73 @@
 import { FieldValue, Timestamp } from "firebase/firestore";
 
-export interface BaseUser {
+// USER (Base, Create, Read, Update)
+
+export interface UserBase {
+  id?: string;
   name: string;
   email: string;
   cpf: string;
   phone: string;
+  role?: UserRole;
 }
 
-export interface UserFirestore extends BaseUser {
-  uid: string;
+export type UserRole = "customer" | "employee" | "manager" | "admin";
+
+export interface UserCreate extends UserBase {
+  id: string;
   points: number;
   totalPointsEarned: number;
   level: "bronze" | "silver" | "gold" | "vip";
+  status: "active" | "inactive" | "suspended" | "deleted";
   createdAt: FieldValue;
   updatedAt: FieldValue;
 }
 
-export interface UserIdentity extends BaseUser {
-  uid: string
+export interface UserRead extends UserBase {
+  id: string;
+  points: number;
+  totalPointsEarned: number;
+  level: "bronze" | "silver" | "gold" | "vip";
+  status: "active" | "inactive" | "suspended" | "deleted";
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
 }
 
-export interface UserForm extends BaseUser {
+export type UserUpdate = Partial<UserRead>;
+
+export interface UserForm extends UserBase {
   password: string;
   confirmPassword: string;
+}
+
+//TRANSACTION (Base, Create, Read, Update)
+export interface TransactionBase {
+  referenceId: string;
+  establismentId?: string;
+  customerId?: string;
+  createdBy: string;
+  createdByType: UserRole;
+  amount: number;
+  source: "purchase" | "reward" | "campaign";
+  type: "earn" | "redeem" | "bonus";
+  version: number;
+}
+
+export interface TransactionCreate extends TransactionBase {
+  createdAt: FieldValue;
+  expiresAt: Timestamp;
+  usedAt: null;
+}
+
+export interface TransactionRead extends TransactionBase {
+  id: string;
+  createdAt: Timestamp;
+  expiresAt: Timestamp;
+  usedAt: Timestamp | null;
+}
+
+export type TransactionUpdate = Partial<TransactionRead>;
+
+export interface TransactionUser extends TransactionRead {
+  qrTransactionId: string;
 }
