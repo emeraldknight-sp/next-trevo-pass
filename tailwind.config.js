43e93fd901ff4c4582201cbc506d1c4e4074026d
David Almeida <contato.davidalmeida@outlook.com>
2026-04-06 10:58:49 -0300

    🔧 Update configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+     "./app/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/kibo-ui/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/magic-ui/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
